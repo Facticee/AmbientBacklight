efa9e2e81b3e6d227a0e1935e48871914f323d12
Facticee <timon.eliah.plum@gmail.com>
2026-08-24 17:25:41 +0200

    switch from blocking readBytes to state machine

--- Firmware/ESP32.ino
+++ Firmware/ESP32.ino
@@ -7,6 +7,10 @@ constexpr uint8_t BRIGHTNESS = 160;
 
 CRGB leds[NUM_LEDS];
 
+enum State { WAIT_A, WAIT_D, WAIT_A2, PIXELS };
+State state = WAIT_A;
+uint16_t receivedBytes = 0;
+
 void setup() {
     FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
     FastLED.setBrightness(BRIGHTNESS);
@@ -14,14 +18,31 @@ void setup() {
     Serial.begin(921600);
 }
 
-void loop() {
-    if (Serial.available() >= 3) {
-        if (Serial.read() == 'A' && Serial.read() == 'd' && Serial.read() == 'a') {
-            size_t loaded = Serial.readBytes((char*)leds, NUM_LEDS * 3);
-
-            if (loaded == NUM_LEDS * 3) {
-                FastLED.show();
+void acceptByte(uint8_t b) {
+    switch (state) {
+        case WAIT_A2: state = (b == 'A') ? WAIT_D : WAIT_A; break;
+        case WAIT_D: state = (b == 'd') ? WAIT_A2 : WAIT_A; break;
+        case WAIT_A:
+            if (b == 'a') {
+                receivedBytes = 0;
+                state = PIXELS;
             }
+
+            else {
+                state = WAIT_A;
+            }
+            break;
+
+        case PIXELS:
+        ((uint8_t*)leds)[receivedBytes++] = b;
+
+        if (receivedBytes == NUM_LEDS * 3) {
+            FastLED.show();
+            state = WAIT_A;
         }
+        break;
     }
+}
+
+void loop() {
 }
