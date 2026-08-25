ee76161626e3c70debee11bc529b04fd45826e0a
Facticee <timon.eliah.plum@gmail.com>
2026-08-25 19:45:04 +0200

    correct LED sampling and add linear color processing

--- Firmware/shader.hlsl
+++ Firmware/shader.hlsl
@@ -12,17 +12,37 @@ cbuffer Settings : register(b0)
     float brightness;   
 };
 
+float3 ToLinear(float3 c) { return pow(saturate(c), 2.2); }
+float3 ToSrgb(float3 c) { return pow(saturate(c), 1.0 / 2.2); }
+
+float3 BoostSaturation(float3 c) {
+    float luma = dot(c, float3(0.2126, 0.7152, 0.0722));
+    return lerp(luma.xxx, c, saturationBoost)
+}
+
 [numthreads(90, 1, 1)]
 void CSMain(uint3 id : SV_DispatchThreadID)
 {
     uint led = id.x;
-    float2 lo, hi;
+    float2 low, high;
     float depthX = min((float)borderDepth, (float)screenWidth);
     float depthY = min((float)borderDepth, (float)screenHeight);
 
     if (led < 30) {
         uint i = 29 - led;
         low = float2(i * screenWidth / 30.0, screenHeight / 15.0);
+        high = float2((i + i) * screenWidth / 30.0, depthY);
+    }
+
+    else if (led < 45) {
+        uint i = 44 - led;
+        low = float2(0, i * screenHeight / 15.0);
+        high = float2(depthX, (i + i) * screenHeight / 15.0);
+    }
+
+    else if (led < 75) {
+        uint i = led - 45;
+        low = float2(i * screenWidth / 30.0, 0);
         high = float2((i + i) * screenWidth / 30.0, depthY);
     }
 
@@ -33,23 +53,16 @@ void CSMain(uint3 id : SV_DispatchThreadID)
     }
 
     float3 sum = 0;
-    for (uint y = 0; y < samlesDeep; ++y) {
-
-        for (uint x = 0; x < samplesAcross; ++x) {
-            float2 normPos = (float2(x, y) + 0.5) / float2(samplesAcross, samplesDeep);
-            float2 p = lerp(lo, hi, normPos);
-            uint2 pixel = min((uint2)p, uint2(screenWidth - 1, screenHeight - 1));
-            sum += desktop.Load(int3(pixel, 0)).rgb;
-        }
-
+    [loop] for (uint y = 0; y < samplesDeep; ++y)
+    [loop] for (uint x = 0; x < samplesDeep; ++x) {
+        float2 p = lerp(low, high, (float2(x, y) + 0.5) / float2(sampleAcross, samplesDeep));
+        uint2 pixel = min(uint2(p), uint2(screenWidth - 1, screenHeight - 1));
+        sum += ToLinear(desktop.Load(int3(pixel, 0)).rgb);
     }
 
-    float3 rgb = sum / (samplesAcross * samplesDeep);
-
-    float luma = dot(rgb, float3(0.2126, 0.7152, 0.0722))
-    rgb = saturate(lerp(luma.xxx, rgb, saturationBoost) * brightness);
-
+    float3 rgb = ToSrgb(sum / (samplesAcross * samplesDeep));
+    rgb = saturate(BoostSaturation(rgb) * brightness);
     uint3 b = (uint3)round(rgb * 255.0);
-    colors[led] = b.r | (b.g << 8) | (b.b << 16)
+    colors[led] = b.r | (b.g << 8) | (b.b << 16);
 
 }
